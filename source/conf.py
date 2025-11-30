3cc5a22f582e7f856a414cfe4b85c51c27ac6123
Bingbing Suo <bsuo@nwu.edu.cn>
2025-11-30 17:36:46 +0800

    Auto-sync: 新增1个文件, 未跟踪2个文件

--- source/conf.py
+++ source/conf.py
@@ -51,7 +51,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh'
+language = 'en'
 
 numfig = True
 
